c6cea64b3002aaa1e9d9e8091a66ed6815f037d8
ZON3DEV <86509465+ZON3DEV@users.noreply.github.com>
2022-03-26 07:39:23 +0100

    Add files via upload

---
+++ contrib/Tools/ClientItem/ClientItem/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace ClientItem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hostBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.portBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tableBox = new System.Windows.Forms.TextBox();
+            this.databaseBox = new System.Windows.Forms.TextBox();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.loginBox = new System.Windows.Forms.TextBox();
+            this.testButt = new System.Windows.Forms.Button();
+            this.okButt = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // hostBox
+            // 
+            this.hostBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.hostBox.Location = new System.Drawing.Point(120, 4);
+            this.hostBox.Name = "hostBox";
+            this.hostBox.Size = new System.Drawing.Size(266, 20);
+            this.hostBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(4, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 26);
+            this.label1.TabIndex = 100;
+            this.label1.Text = "Host: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(4, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 26);
+            this.label2.TabIndex = 100;
+            this.label2.Text = "Login: ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(4, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 26);
+            this.label3.TabIndex = 100;
+            this.label3.Text = "Password: ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(4, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 26);
+            this.label4.TabIndex = 100;
+            this.label4.Text = "World database: ";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(4, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 26);
+            this.label5.TabIndex = 100;
+            this.label5.Text = "Item_template table: ";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 115F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.portBox, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label10, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.tableBox, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.databaseBox, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.passBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.loginBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.testButt, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.hostBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.okButt, 1, 6);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(390, 202);
+            this.tableLayoutPanel1.TabIndex = 100;
+            // 
+            // portBox
+            // 
+            this.portBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.portBox.Location = new System.Drawing.Point(120, 139);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(266, 20);
+            this.portBox.TabIndex = 5;
+            this.portBox.Text = "3306";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label10.Location = new System.Drawing.Point(4, 136);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(109, 26);
+            this.label10.TabIndex = 101;
+            this.label10.Text = "Port: ";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tableBox
+            // 
+            this.tableBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableBox.Location = new System.Drawing.Point(120, 112);
+            this.tableBox.Name = "tableBox";
+            this.tableBox.Size = new System.Drawing.Size(266, 20);
+            this.tableBox.TabIndex = 4;
+            this.tableBox.Text = "item_template";
+            // 
+            // databaseBox
+            // 
+            this.databaseBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.databaseBox.Location = new System.Drawing.Point(120, 85);
+            this.databaseBox.Name = "databaseBox";
+            this.databaseBox.Size = new System.Drawing.Size(266, 20);
+            this.databaseBox.TabIndex = 3;
+            this.databaseBox.Text = "world";
+            // 
+            // passBox
+            // 
+            this.passBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.passBox.Location = new System.Drawing.Point(120, 58);
+            this.passBox.Name = "passBox";
+            this.passBox.Size = new System.Drawing.Size(266, 20);
+            this.passBox.TabIndex = 2;
+            this.passBox.UseSystemPasswordChar = true;
+            // 
+            // loginBox
+            // 
+            this.loginBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.loginBox.Location = new System.Drawing.Point(120, 31);
+            this.loginBox.Name = "loginBox";
+            this.loginBox.Size = new System.Drawing.Size(266, 20);
+            this.loginBox.TabIndex = 1;
+            // 
+            // testButt
+            // 
+            this.testButt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.testButt.Location = new System.Drawing.Point(4, 166);
+            this.testButt.Name = "testButt";
+            this.testButt.Size = new System.Drawing.Size(109, 32);
+            this.testButt.TabIndex = 6;
+            this.testButt.Text = "Test connection";
+            this.testButt.UseVisualStyleBackColor = true;
+            this.testButt.Click += new System.EventHandler(this.testButt_Click);
+            // 
+            // okButt
+            // 
+            this.okButt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.okButt.Location = new System.Drawing.Point(120, 166);
+            this.okButt.Name = "okButt";
+            this.okButt.Size = new System.Drawing.Size(266, 32);
+            this.okButt.TabIndex = 7;
+            this.okButt.Text = "Get data";
+            this.okButt.UseVisualStyleBackColor = true;
+            this.okButt.Click += new System.EventHandler(this.okButt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 202);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Client Item";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox hostBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox tableBox;
+        private System.Windows.Forms.TextBox databaseBox;
+        private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.TextBox loginBox;
+        private System.Windows.Forms.Button testButt;
+        private System.Windows.Forms.Button okButt;
+        private System.Windows.Forms.TextBox portBox;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ contrib/Tools/ClientItem/ClientItem/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
+using System.Globalization;
+
+namespace ClientItem
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection connection = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void testButt_Click(object sender, EventArgs e)
+        {
+            TestConnection();
+        }
+
+        private void okButt_Click(object sender, EventArgs e)
+        {
+            DownloadData();
+        }
+
+        private void CreateConnection()
+        {
+            if (connection == null)
+            {
+                string con = "Server=" + hostBox.Text + "; Port=" + portBox.Text + "; Database=" + databaseBox.Text + "; Uid=" + loginBox.Text + "; Pwd=" + passBox.Text + ";";
+
+                try
+                {
+                    connection = new MySqlConnection(con);
+                    connection.Open();
+                }
+                catch (MySqlException e)
+                {
+                    MessageBox.Show("Connection wasn't succesful. Error: " + e.Number + "\n\n" + e.ToString());
+                    connection = null;
+                }
+            }
+        }
+
+        private void TestConnection()
+        {
+            CreateConnection();
+            if (connection != null)
+                MessageBox.Show("Connection was successful.");
+        }
+
+        private void DownloadData()
+        {
+            CreateConnection();
+
+            if (connection != null)
+            {
+                StreamWriter swDBC = new StreamWriter("Item.dbc.csv", false);
+                StreamWriter swWMV = new StreamWriter("items.csv", false);
+                swDBC.WriteLine("long,long,long,flags,long,long,long,long,");
+                try
+                {
+                    string commText = "SELECT entry, class, subclass, SoundOverrideSubclass, Material, displayid, InventoryType, sheath, Quality, name FROM " + tableBox.Text + " ORDER BY entry ASC;";
+                    MySqlCommand command = new MySqlCommand(commText, connection);
+                    MySqlDataReader r = command.ExecuteReader();
+
+                    while (r.Read())
+                    {
+                        var normalizedString = ("" + r[9]).Normalize(NormalizationForm.FormD);
+                        var stringBuilder = new StringBuilder();
+
+                        foreach (var c in normalizedString)
+                        {
+                            var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                            if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                            {
+                                stringBuilder.Append(c);
+                            }
+                        }
+
+                        swDBC.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},", r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7]);
+                        swWMV.WriteLine("{0},{1},{2}", r[0], r[8], stringBuilder.ToString().Normalize(NormalizationForm.FormC));
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Error has occured:\n" + e.Message);
+                }
+
+                MessageBox.Show("All successfully done!");
+                swWMV.Close();
+                swDBC.Close();
+                connection.Close();
+            }
+        }
+    }
+}
