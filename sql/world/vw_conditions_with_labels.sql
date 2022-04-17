cc43fced06b97164d02e197a1de91fb28fbc2260
ZON3DEV <86509465+ZON3DEV@users.noreply.github.com>
2022-04-17 19:53:27 +0200

    Add files via upload

--- sql/world/vw_conditions_with_labels.sql
+++ sql/world/vw_conditions_with_labels.sql
@@ -70,4 +70,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-14  6:40:34
+-- Dump completed on 2022-04-17 18:40:10
--- sql/world/vw_disables_with_labels.sql
+++ sql/world/vw_disables_with_labels.sql
@@ -61,4 +61,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-14  6:40:35
+-- Dump completed on 2022-04-17 18:40:11
