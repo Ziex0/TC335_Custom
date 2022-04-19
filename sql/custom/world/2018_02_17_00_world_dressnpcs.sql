7e89c9c1c028fc8fac0dbaf780152ffd36da32d8
ZON3DEV <86509465+ZON3DEV@users.noreply.github.com>
2022-04-19 02:28:30 +0200

    Update 2018_02_17_00_world_dressnpcs.sql

--- sql/custom/world/2018_02_17_00_world_dressnpcs.sql
+++ sql/custom/world/2018_02_17_00_world_dressnpcs.sql
@@ -1,20 +1,5 @@
-ALTER TABLE `creature_template` CHANGE COLUMN `modelid1` `modelid1` BIGINT;
-ALTER TABLE `creature_template` CHANGE COLUMN `modelid2` `modelid2` BIGINT;
-ALTER TABLE `creature_template` CHANGE COLUMN `modelid3` `modelid3` BIGINT;
-ALTER TABLE `creature_template` CHANGE COLUMN `modelid4` `modelid4` BIGINT;
-UPDATE creature_template SET modelid1 = (-modelid1) + 3000000000 WHERE modelid1 < 0;
-UPDATE creature_template SET modelid2 = (-modelid2) + 3000000000 WHERE modelid2 < 0;
-UPDATE creature_template SET modelid3 = (-modelid3) + 3000000000 WHERE modelid3 < 0;
-UPDATE creature_template SET modelid4 = (-modelid4) + 3000000000 WHERE modelid4 < 0;
-ALTER TABLE `creature_template` CHANGE COLUMN `modelid1` `modelid1` INT(10) UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `creature_template` CHANGE COLUMN `modelid2` `modelid2` INT(10) UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `creature_template` CHANGE COLUMN `modelid3` `modelid3` INT(10) UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `creature_template` CHANGE COLUMN `modelid4` `modelid4` INT(10) UNSIGNED NOT NULL DEFAULT '0';
+REPLACE INTO `creature_template` VALUES (95030, 0, 0, 0, 0, 0, 30072, 0, 0, 0, 'Jeanine Blutaxt', 'Handelsverbund', 'Buy', 57025, 80, 80, 0, 35, 129, 1, 1.14286, 1, 0, 0, 1500, 0, 1, 1, 1, 512, 2048, 8, 0, 7, 138412032, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 2, '', 0);
 
-ALTER TABLE `creature_template_outfits` DROP PRIMARY KEY;
-UPDATE creature_template_outfits SET entry = entry + 3000000000 WHERE entry <= 0x7FFFFFFF;
-ALTER TABLE `creature_template_outfits` ADD PRIMARY KEY (`entry`);
-
-ALTER TABLE `creature` CHANGE COLUMN `modelid` `modelid` INT(10) UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `game_event_model_equip` CHANGE COLUMN `modelid` `modelid` INT(10) UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `creature_model_info` CHANGE COLUMN `DisplayID` `DisplayID` INT(10) UNSIGNED NOT NULL DEFAULT '0';
+REPLACE INTO `creature_template_outfits` (`entry`, `race`, `gender`, `skin`, `face`, `hair`, `haircolor`, `facialhair`, `head`, `shoulders`, `body`, `chest`, `waist`, `legs`, `feet`, `wrists`, `hands`, `back`, `tabard`)
+VALUES (3000000123, 11, 1, 14, 4, 10, 3, 5, -31286, -43617, 0, -26267, -26270, -26272, 0, 0, -43698, 0, 0);
+UPDATE `creature_template` SET `modelid2` = 3000000123 WHERE `entry` = 95030;
